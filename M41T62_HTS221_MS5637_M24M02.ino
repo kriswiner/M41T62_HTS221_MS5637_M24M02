b1d3f6b1295a03c38f79d1111cc735bd5a673060
Kris Winer <kriswiner@users.noreply.github.com>
2015-01-27 19:41:32 -0800

    Update M41T62_HTS221_MS5637_M24M02.ino

--- M41T62_HTS221_MS5637_M24M02.ino
+++ M41T62_HTS221_MS5637_M24M02.ino
@@ -20,7 +20,7 @@
  SDA and SCL should have external pull-up resistors (to 3.3V).
  4k7 resistors are on the M41T62+MS5637+M24512DFM Mini Add-On board for Teensy 3.1.
  
- The M41T62+MS5637+HTS221+M24M02DFC board is so low power it can run off of digitalWrite on any GPIO pin. On the Teensy 3.1, we
+ The M41T62+MS5637+HTS221+M24M02DFC board is so low power it can run off of digitalWrite on any GPIO pin. On the Teensy 3.1:
  
  Hardware setup:
  M41T62 Mini Add-On ------- Teensy 3.1
